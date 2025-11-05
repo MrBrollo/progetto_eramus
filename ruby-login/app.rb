316cbbc064a878ec2a98b2f6b5b434d65e184729
MrBrollo <147092556+MrBrollo@users.noreply.github.com>
2025-11-05 16:29:26 +0100

    aggiunta cifrature delle password

--- ruby-login/app.rb
+++ ruby-login/app.rb
@@ -1,8 +1,8 @@
 require "pg"
 require "json"
 require "webrick"
+require "bcrypt"
 require_relative "db_connection"
-$stderr.sync = true
 
 PORT = 4567
 
@@ -16,11 +16,7 @@ server = WEBrick::HTTPServer.new(
     AccessLog: access_log
 )
 
-server.mount_proc("/") do |req, res|
-    res["Content-Type"] = "text/html"
-    res.body = "<h1>Server Attivo</h1><p>Usa POST /login</p>"
-end
-
+# Endpoint di LOGIN
 server.mount_proc "/login" do |req, res|
     res["Content-Type"] = "application/json"
     res["Access-Control-Allow-Origin"] = "*"
@@ -40,37 +36,100 @@ server.mount_proc "/login" do |req, res|
 
         if username.nil? || password.nil? || username.strip.empty? || password.strip.empty?
             res.status = 400
+            res.body = { success: false, message: "Inserisci tutti i campi obbligatori" }
+            next
+        end
+
+        conn = db_connection
+
+        result = conn.exec_params("SELECT * FROM utente WHERE username = $1 LIMIT 1", [username])
+
+        if result.ntuples > 0
+            utente = result[0]
+            hashed_password = utente["password"]
+
+            if BCrypt::Password.new(hashed_password) == password
+                res.status = 200
+                res.body = {
+                    success: true,
+                    message: "Accesso effettuato con successo!",
+                    utente: {
+                        nome: utente["nome"],
+                        cognome: utente["cognome"]
+                        username: utente["username"]
+                    }
+                }.to_json
+            else
+                res.status = 401
+                res.body = { success: false, message: "Credenziali non valide" }.to_json
+            end
+        else
+            res.status = 401
+            res.body = { success: false, message: "Utente non trovato" }.to_json
+        end
+
+    rescue JSON::ParseError
+        res.status = 400
+        res.body = { success: false, message: "Formato JSON non valido" }.to_json
+    rescue PG:Error => e
+        res.status = 400
+        res.body = { success: false, message: "Errore database: #{e.message}" }.to_json
+    ensure
+        conn.close if conn
+    end
+end
+
+#Endpoint di REGISTRAZIONE
+server.mount_proc "/register" do |req, res|
+    res["Content-Type"] = "application/json"
+    res["Access-Control-Allow-Origin"] = "*"
+    res["Access-Control-Allow-Methods"] = "POST, OPTIONS"
+    res["Access-Control-Allow-Headers"] = "Content-Type"
+
+    if req.request_method == "OPTIONS"
+        res.status = 200
+        next
+    end
+
+    begin
+        data = JSON.parse(req.body)
+
+        username = data["username"]
+        password = data["password"]
+        nome = data["nome"]
+        cognome = data["cognome"]
+
+        if username.nil? || password.nil? || nome.nil? || cognome.nil? || username.strip.empty? || password.strip.empty?
+            res.status = 400
             res.body = { success: false, message: "Inserisci tutti i campi obbligatori" }.to_json
             next
         end
 
         conn = db_connection
 
-        result = conn.exec_params("SELECT * FROM utente WHERE username = $1 AND password = $2 LIMIT 1", [username, password])
-
-        if result.ntuples > 0
-            utente = result[0]
-            res.status = 200
-            res.body = {
-                success: true,
-                message: "Accesso effettuato con successo!",
-                utente: {
-                    nome: utente["nome"],
-                    cognome: utente["cognome"],
-                    username: utente["username"]
-                }
-            }.to_json
-        else
-            res.status = 401
-            res.body = { success:false, message: "Credenziali non valide" }.to_json
+        check = conn.exec_params("SELECT * FROM utente WHERE username = $1", [username])
+        if check.ntuples > 0
+            res.status = 409
+            res.body = { success: false, message: "Username già esistente" }.to_json
+            next
         end
+
+        hashed_password = BCrypt::Password.create(password)
+
+        conn.exec_params(
+            "INSERT INTO utente (username, password, nome, cognome) VALUES ($1, $2, $3, $4)", [username, password, nome, cognome]
+        )
+
+        res.status = 201
+        res.body = {success: true, message: "Utente registrato con successo" }.to_json
 
     rescue JSON::ParseError
         res.status = 400
         res.body = { success: false, message: "Formato JSON non valido" }.to_json
-    rescue PG::Error => e
+
+    rescue PG::ParseError
         res.status = 400
-        res.body = { success: false, message:"Errore database: #{e.message}" }.to_json
+        res.body = { success: false, message: "Errore database #{e.message}" }.to_json
     ensure
         conn.close if conn
     end
